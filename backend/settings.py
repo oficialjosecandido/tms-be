016090aab948372d1a3f4179fed6b30e0e252886
JOSE CANDIDO <jose.candido_ext@a-to-be.com>
2024-05-09 17:39:20 +0200

    create gitignore

--- backend/settings.py
+++ backend/settings.py
@@ -11,6 +11,14 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from .secrets import (
+    AWS_ACCESS_KEY_ID,
+    AWS_SECRET_ACCESS_KEY,
+    AWS_STORAGE_BUCKET_NAME,
+    AWS_S3_REGION_NAME,
+    EMAIL_HOST_USER,
+    EMAIL_HOST_PASSWORD,
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -141,10 +149,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #  STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 # Media files configuration
-AWS_ACCESS_KEY_ID = 'AKIAVRUVVSYZQMLU44M4'
-AWS_SECRET_ACCESS_KEY = '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'
-AWS_STORAGE_BUCKET_NAME = 'trademyspin-dev'
-AWS_S3_REGION_NAME = 'eu-north-1'
+
 AWS_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 AWS_S3_VERIFY = True
