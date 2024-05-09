0e63d2dcde3914ed90083c7fb57f90dd5e1e3ee8
JOSE CANDIDO <jose.candido_ext@a-to-be.com>
2024-05-09 17:42:33 +0200

    add secrets

--- backend/settings.py
+++ backend/settings.py
@@ -158,8 +158,7 @@ AWS_S3_VERIFY = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp-pt.securemail.pro'
 EMAIL_PORT = 465
-EMAIL_HOST_USER = 'secretaria@tejomag.pt'
-EMAIL_HOST_PASSWORD = 'dGxP7ytYD_$-ngR'
+
 EMAIL_USE_TLS = False
 EMAIL_USE_SSL = True
 
