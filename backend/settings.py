d9606ae1e4ced3c61f4ffa67bfd33d1d10aeca8f
JOSE CANDIDO <jose.candido_ext@a-to-be.com>
2024-05-21 23:18:01 +0200

    fix build

--- backend/settings.py
+++ backend/settings.py
@@ -138,6 +138,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 APPEND_SLASH=False
 
 # Default primary key field type
